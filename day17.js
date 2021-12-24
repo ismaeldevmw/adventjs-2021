2b21c27bef764c244b9dd94f488fdb9d2e88e27b
Ismael López <ismaellopez.dev@gmail.comm>
2021-12-23 18:05:35 -0600

    :sparkles: Solution day 17

---
+++ day17.js
@@ -0,0 +1,38 @@
+function countPackages(carriers, carrierID) {
+  const cb = (carriers = [], carriersIDs = []) => {
+    return carriersIDs.reduce((acc, carrierID = '') => {
+      return (
+        acc +
+        carriers.reduce((acc, carrier = []) => {
+          if (carrier[0] === carrierID) {
+            return acc + carrier[1] + cb(carriers, carrier[2]);
+          }
+          return acc;
+        }, 0)
+      );
+    }, 0);
+  };
+  return cb(carriers, [carrierID]);
+}
+
+const carriers = [
+  ['dapelu', 5, ['midu', 'jelowing']],
+  ['midu', 2, []],
+  ['jelowing', 2, []],
+];
+
+console.log(countPackages(carriers, 'dapelu')); // 9
+// 5 de dapelu, 2 de midu y 2 de jelowing = 9
+
+const carriers2 = [
+  ['lolivier', 8, ['camila', 'jesuspoleo']],
+  ['camila', 5, ['sergiomartinez', 'conchaasensio']],
+  ['jesuspoleo', 4, []],
+  ['sergiomartinez', 4, []],
+  ['conchaasensio', 3, ['facundocapua', 'faviola']],
+  ['facundocapua', 2, []],
+  ['faviola', 1, []],
+];
+
+console.log(countPackages(carriers2, 'camila')); // 15
+// 5 de camila, 4 de sergiomartinez, 3 de conchaasensio, 2 de facundocapua y 1 de faviola = 15
